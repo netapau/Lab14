f4ece81aa96deeaa1d75ec465075b4bdf1ea8749
Tony Simoes <sr.tony@gmail.com>
2017-06-26 11:16:48 +0200

    Goto implementar logica de validacion

--- Lab14/Resources/Resource.Designer.cs
+++ Lab14/Resources/Resource.Designer.cs
@@ -26,6 +26,43 @@ namespace Lab14
 		
 		public static void UpdateIdValues()
 		{
+			global::Xamarin.Auth._MobileServices.Resource.Animation.slide_in_right = global::Lab14.Resource.Animation.slide_in_right;
+			global::Xamarin.Auth._MobileServices.Resource.Animation.slide_out_left = global::Lab14.Resource.Animation.slide_out_left;
+			global::Xamarin.Auth._MobileServices.Resource.Drawable.ic_arrow_back = global::Lab14.Resource.Drawable.ic_arrow_back;
+			global::Xamarin.Auth._MobileServices.Resource.Id.webview = global::Lab14.Resource.Id.webview;
+			global::Xamarin.Auth._MobileServices.Resource.Layout.activity_webview = global::Lab14.Resource.Layout.activity_webview;
+			global::Xamarin.Auth._MobileServices.Resource.String.ApplicationName = global::Lab14.Resource.String.ApplicationName;
+			global::Xamarin.Auth._MobileServices.Resource.String.Hello = global::Lab14.Resource.String.Hello;
+			global::Xamarin.Auth._MobileServices.Resource.String.title_activity_webview = global::Lab14.Resource.String.title_activity_webview;
+			global::PCLCrypto.Resource.String.ApplicationName = global::Lab14.Resource.String.ApplicationName;
+			global::PCLCrypto.Resource.String.Hello = global::Lab14.Resource.String.Hello;
+			global::SALLab14.Resource.Animation.slide_in_right = global::Lab14.Resource.Animation.slide_in_right;
+			global::SALLab14.Resource.Animation.slide_out_left = global::Lab14.Resource.Animation.slide_out_left;
+			global::SALLab14.Resource.Drawable.ic_arrow_back = global::Lab14.Resource.Drawable.ic_arrow_back;
+			global::SALLab14.Resource.Id.webview = global::Lab14.Resource.Id.webview;
+			global::SALLab14.Resource.Layout.activity_webview = global::Lab14.Resource.Layout.activity_webview;
+			global::SALLab14.Resource.String.ApplicationName = global::Lab14.Resource.String.ApplicationName;
+			global::SALLab14.Resource.String.Hello = global::Lab14.Resource.String.Hello;
+			global::SALLab14.Resource.String.title_activity_webview = global::Lab14.Resource.String.title_activity_webview;
+		}
+		
+		public partial class Animation
+		{
+			
+			// aapt resource value: 0x7f040000
+			public const int slide_in_right = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int slide_out_left = 2130968577;
+			
+			static Animation()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Animation()
+			{
+			}
 		}
 		
 		public partial class Attribute
@@ -45,16 +82,19 @@ namespace Lab14
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int ic_arrow_back = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int XamarinDiplomado = 2130837505;
+			public const int Icon = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int XamarinDiplomadoH = 2130837506;
+			public const int XamarinDiplomado = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int XamarinDiplomadoM = 2130837507;
+			public const int XamarinDiplomadoH = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int XamarinDiplomadoM = 2130837508;
 			
 			static Drawable()
 			{
@@ -69,23 +109,32 @@ namespace Lab14
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050005
-			public const int buttonValidate = 2131034117;
+			// aapt resource value: 0x7f060006
+			public const int buttonValidate = 2131099654;
 			
-			// aapt resource value: 0x7f050002
-			public const int editTextEmail = 2131034114;
+			// aapt resource value: 0x7f060003
+			public const int editTextEmail = 2131099651;
 			
-			// aapt resource value: 0x7f050004
-			public const int editTextPassword = 2131034116;
+			// aapt resource value: 0x7f060005
+			public const int editTextPassword = 2131099653;
 			
-			// aapt resource value: 0x7f050000
-			public const int imageViewLogo = 2131034112;
+			// aapt resource value: 0x7f060001
+			public const int imageViewLogo = 2131099649;
 			
-			// aapt resource value: 0x7f050001
-			public const int textViewEmail = 2131034113;
+			// aapt resource value: 0x7f060007
+			public const int linearLayout1 = 2131099655;
 			
-			// aapt resource value: 0x7f050003
-			public const int textViewPassword = 2131034115;
+			// aapt resource value: 0x7f060008
+			public const int linearLayout2 = 2131099656;
+			
+			// aapt resource value: 0x7f060002
+			public const int textViewEmail = 2131099650;
+			
+			// aapt resource value: 0x7f060004
+			public const int textViewPassword = 2131099652;
+			
+			// aapt resource value: 0x7f060000
+			public const int webview = 2131099648;
 			
 			static Id()
 			{
@@ -101,7 +150,10 @@ namespace Lab14
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int activity_webview = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
 			
 			static Layout()
 			{
@@ -116,11 +168,14 @@ namespace Lab14
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			// aapt resource value: 0x7f050001
+			public const int ApplicationName = 2131034113;
 			
-			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			// aapt resource value: 0x7f050000
+			public const int Hello = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int title_activity_webview = 2131034114;
 			
 			static String()
 			{
