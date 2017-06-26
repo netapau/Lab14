acf4a1a396ba82d8f1b1835cc5cb1891b53ba5d7
Tony Simoes <sr.tony@gmail.com>
2017-06-26 10:48:15 +0200

    Go to validation

--- Lab14/Resources/Resource.Designer.cs
+++ Lab14/Resources/Resource.Designer.cs
@@ -53,6 +53,9 @@ namespace Lab14
 			// aapt resource value: 0x7f020002
 			public const int XamarinDiplomadoH = 2130837506;
 			
+			// aapt resource value: 0x7f020003
+			public const int XamarinDiplomadoM = 2130837507;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
