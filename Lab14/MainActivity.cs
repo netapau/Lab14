6d08f2bff7440bdf2ea76654783db5a09844083e
Tony Simoes <sr.tony@gmail.com>
2017-06-26 13:04:31 +0200

    Validacion efectuada

--- Lab14/MainActivity.cs
+++ Lab14/MainActivity.cs
@@ -7,12 +7,37 @@ namespace Lab14
     [Activity(Label = "Lab14", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
             SetContentView (Resource.Layout.Main);
+
+            var ValidacionButton = FindViewById<Button>(Resource.Id.buttonValidate);
+
+            ValidacionButton.Click += (sender, ev) => {
+
+                ValidacionAsync();
+
+            };
+
+        }
+
+        private async void ValidacionAsync()
+        {
+            var Client = new SALLab14.ServiceClient();
+            var Result = await Client.ValidateAsync(this);
+
+            Android.App.AlertDialog.Builder Builder =
+            new AlertDialog.Builder(this);
+            AlertDialog Alert = Builder.Create();
+
+            Alert.SetTitle("Resultado de la verificación");
+            Alert.SetIcon(Resource.Drawable.Icon);
+            Alert.SetMessage($"{Result.Status}\n{Result.FullName}\n{Result.Token}");
+            Alert.Show();
         }
     }
 }
